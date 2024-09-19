c9d2863ac52c1bf384b6cf8f03d98410968c00f6
Saurav Sabu <saurav.sabu9@gmail.com>
2024-09-19 22:38:58 +0530

    updated requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+# Setup function to handle package metadata and configuration
+setup(
+    name = "Titanic-Survival-Analysis",
+    version = "0.0.1",
+    author="Saurav Sabu",
+    author_email="saurav.sabu9@gmail.com",
+    packages = find_packages() # Automatically find and include all packages in the directory
+)
